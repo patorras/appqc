6628ef2fb249ae7c43dc8fcfc38cb5c153f911c1
patorras <triotank@gmail.com>
2020-04-22 15:57:49 +0100

    update

--- gui.py
+++ gui.py
@@ -4,7 +4,26 @@ root = Tk()
 
 root.title("Samples entry")
 
-e = Entry(root, width=35)
-e.pack()
+# batch label
+label_batch = Label(root, text="Batch")
+label_batch.grid(row=0, column=0)
+
+# label for moment
+label_moment = Label(root, text="Moment")
+label_moment.grid(row=0, column=1)
+
+
+# batch entry box
+e_batch = Entry(root, width=35)
+e_batch.grid(row=1, column=0)
+
+# moment entry box
+e_moment = Entry(root, width=35)
+e_moment.grid(row=1, column=1)
+
+
+# button
+ok_button = Button(root, width=35, text="OK")
+ok_button.grid(row=2, column=0)
 
 root.mainloop()
