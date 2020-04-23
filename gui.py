8e150570db5a5231adc0538d4c7c69c683dfe5f0
patorras <triotank@gmail.com>
2020-04-23 17:26:35 +0100

    pass to windows

--- gui.py
+++ gui.py
@@ -21,7 +21,7 @@ root.geometry("500x600")
         #date datetime)""")
 
 
-
+#########################################################################################################################################
 # function to submit entrys
 def submit():
     # connects to database
@@ -51,9 +51,42 @@ def submit():
     # deletes the entry box
     e_batch.delete(0, END)
 
+##############################################################################################################################################
+
+def edit():
+    second = Tk()
+    second.geometry("500x600")
+    second.title("Edit Window")
+
+    # label
+    label_id = Label(second, text="ID")
+    label_id.grid(row=0, column=0)
+
+    label_batch = Label(second, text="Batch")
+    label_batch.grid(row=1, column=0)
+
+    label_moment = Label(second, text="Moment")
+    label_moment.grid(row=2, column=0)
+
+    label_date = Label(second, text="Date")
+    label_date.grid(row=3, column=0)
+
+    # Entry boxes
+    e_id = Entry(second, width=35)
+    e_id.grid(row=0, column=1)
+
+    e_batch = Entry(second, width=35)
+    e_batch.grid(row=1, column=1)
+
+    e_moment = Entry(second, width=35)
+    e_moment.grid(row=2, column=1)
+
+    e_date = Entry(second, width=35)
+    e_date.grid(row=3, column=1)
 
 
 
+##############################################################################################################################################
 
 # batch label
 label_batch = Label(root, text="Batch")
@@ -77,6 +110,12 @@ drop.grid(row=0, column=2, pady=(20, 0))
 # submit button
 submit_button = Button(root, text="Submit", command=submit)
 submit_button.grid(row=1, columnspan=3, pady=(10, 0))
+
+# Edit
+edit = Button(root, text="Edit", command=edit)
+edit.grid(row=3, columnspan=3, pady=(10, 0))
+
+
 
 
 root.mainloop()
